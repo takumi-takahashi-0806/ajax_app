a0eeddc90ab29d4938c4345a55047100e09ba1a7
takumi-takahashi-0806 <coyote0039@gmail.com>
2020-10-20 17:48:47 +0900

    AjaxAppの準備

--- db/migrate/20200929034017_create_posts.rb
+++ db/migrate/20200929034017_create_posts.rb
@@ -2,7 +2,8 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.text :content
-      t.timestampsmodel 
+      t.boolean :checked
+      t.timestamps
     end
   end
 end
